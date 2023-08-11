e26f200799314158e190b2850a0fffe34f730358
Yicheng Luo <ethanluoyc@gmail.com>
2023-08-11 19:36:24 +0100

    Add parameter sweep example

---
+++ examples/parameter_sweep/py_package/main.py
@@ -0,0 +1,13 @@
+import os
+from absl import app
+from absl import flags
+
+_SEED = flags.DEFINE_integer("seed", 0, "Random seed")
+
+
+def main(_):
+    print("--seed", _SEED.value, "TASK", os.environ["TASK"])
+
+
+if __name__ == "__main__":
+    app.run(main)
