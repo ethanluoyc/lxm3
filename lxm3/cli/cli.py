a74a42bf00bfedac5662d17573d01788e39634bb
Yicheng Luo <ethanluoyc@gmail.com>
2023-08-11 19:09:07 +0100

    Improve CLI arguments handling

--- lxm3/cli/cli.py
+++ lxm3/cli/cli.py
@@ -1,38 +1,91 @@
 #!/usr/bin/env python3
+import argparse
 import errno
 import importlib
 import os
 import sys
 
 from absl import app
+from absl.flags import argparse_flags
+
+import lxm3
 
 
-def main(argv):
-    cmd = argv[1]
-    if cmd == "version":
-        from lxm3 import __version__
+def version(_):
+    print(f"lxm3 {lxm3.__version__}")
 
-        print(__version__)
-        sys.exit(0)
-    if len(argv) < 3:
-        raise app.UsageError("There must be at least 2 command-line arguments")
-    if cmd == "launch":
-        launch_script = argv[2]
-        if not os.path.exists(launch_script):
-            raise OSError(errno.ENOENT, f"File not found: {launch_script}")
-        sys.path.insert(0, os.path.abspath(os.path.dirname(launch_script)))
-        launch_module, _ = os.path.splitext(os.path.basename(launch_script))
-        m = importlib.import_module(launch_module)
-        # fmt:off
-        argv = [launch_script, "--xm_launch_script={}".format(launch_script)] + argv[3:]
-        app.run(m.main, argv=argv)
-        sys.path.pop(0)
-        # fmt:on
+
+def register_version_parser(parsers: argparse._SubParsersAction):
+    version_parser = parsers.add_parser(
+        "version",
+        help="Print version.",
+        inherited_absl_flags=None,
+    )
+    version_parser.set_defaults(command=version)
+
+
+def launch(args):
+    launch_script = args.launch_script
+    if not os.path.exists(launch_script):
+        raise OSError(errno.ENOENT, f"File not found: {launch_script}")
+    sys.path.insert(0, os.path.abspath(os.path.dirname(launch_script)))
+    launch_module, _ = os.path.splitext(os.path.basename(launch_script))
+    m = importlib.import_module(launch_module)
+    argv = [launch_script, "--xm_launch_script={}".format(launch_script)] + args.args
+    app.run(m.main, argv=argv)
+    sys.path.pop(0)
+
+
+def register_launch_parser(parsers: argparse._SubParsersAction):
+    launch_parser = parsers.add_parser(
+        "launch",
+        help="Launch experiment.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        inherited_absl_flags=None,
+        epilog=r"""
+examples:
+
+  Launch experiment defined in "launcher.py"
+  and pass extra args "--task 1" to "launcher.py".
+
+    lxm3 launch launcher.py -- --task 1
+""",
+    )
+    launch_parser.add_argument(
+        "launch_script",
+        metavar="LAUNCH_SCRIPT",
+        # nargs=1,
+        help="Path to launch script.",
+    )
+    launch_parser.add_argument(
+        "args",
+        nargs=argparse.REMAINDER,
+        metavar="ARGS",
+        help="Additional arguments to pass to launch script.",
+    )
+    launch_parser.set_defaults(command=launch)
+
+
+def _parse_flags(argv):
+    parser = argparse_flags.ArgumentParser(description="lxm3 experiment scheduler.")
+    parser.set_defaults(command=lambda _: parser.print_help())
+
+    subparsers = parser.add_subparsers()
+
+    register_version_parser(subparsers)
+    register_launch_parser(subparsers)
+
+    args = parser.parse_args(argv[1:])
+    return args
+
+
+def main(args):
+    args.command(args)
 
 
 def entrypoint():
-    app.run(main)
+    app.run(main, flags_parser=_parse_flags)
 
 
-if __name__ == "__all__":
-    app.run(main)
+if __name__ == "__main__":
+    entrypoint()
